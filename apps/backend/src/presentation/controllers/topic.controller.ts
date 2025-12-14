664d7ae01631984162b4397cdb24a9e0a6a1d926
Mohammed Raihan <aizvanr@gmail.com>
2025-12-14 13:31:50 +0530

    feat: skelton for topic controller

---
+++ apps/backend/src/presentation/controllers/topic.controller.ts
@@ -0,0 +1,42 @@
+/*
+Endpoints to implement:
+
+POST /topics → create topic
+GET /topics → list topics
+POST /topics/:id/vote → vote
+DELETE /topics/:id → delete
+
+Controller rules:
+
+Extract params/body
+Pass userId (mock/session for now)
+Call service
+Send JSON response
+Catch errors
+*/
+
+
+import { Request, Response } from "express";
+import { TopicService } from "../../application/services/topic.service";
+
+export class TopicController {
+    constructor(private topicService: TopicService) {
+
+    }
+
+    async createTopic(req: Request, res: Response) {
+        
+    }
+
+    async getAllTopics(req: Request, res: Response) {
+
+    }
+
+    async vote(req: Request, res: Response) {
+
+    }
+    
+    async deleteTopic(req: Request, res: Response) {
+
+    }
+}
