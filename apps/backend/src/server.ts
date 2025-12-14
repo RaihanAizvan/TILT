251ca7f6dfddc0636cc0ba4ea5390b0830bc0023
Mohammed Raihan <aizvanr@gmail.com>
2025-12-14 23:01:41 +0530

    feat: create connection to mongo

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,21 @@
 import app from "./app.js";
+import { connectMongo } from "./infrastructure/database/mongo.js";
 
 const PORT = Number(process.env.PORT || 3000);
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+
+async function startServer(): Promise<void> {
+  try {
+    await connectMongo();
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+
+  } catch (err:any) {
+    console.log("failed to start mongo server", err.message)
+    process.exit(1)
+  }
+}
+
+
+startServer();
